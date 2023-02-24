7b682fec003ca9a980c48e390b5ef35ace2ed7d6
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-02-24 02:57:43 +0100

    a

--- server/index.js
+++ server/index.js
@@ -1,51 +1,46 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import env from 'dotenv'
-import { Configuration, OpenAIApi } from "openai";
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 
-const app = express()
+const app = express();
 
-env.config()
+env.config();
 
-app.use(cors())
-app.use(bodyParser.json())
-
+app.use(cors());
+app.use(bodyParser.json());
 
 // Configure open api
 const configuration = new Configuration({
-    organization: "org-3rJT9Z2GDSeSjJhLoD6NJ1U3",
-    apiKey: process.env.API_KEY 
-})
-const openai = new OpenAIApi(configuration)
-console.log(configuration)
-
+  organization: 'org-3rJT9Z2GDSeSjJhLoD6NJ1U3',
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+console.log(configuration);
 
 // listeninng
-app.listen("3080", ()=>console.log("listening on port 3080"))
-
+app.listen('3080', () => console.log('listening on port 3080'));
 
 // dummy route to test
-app.get("/", (req, res) => {
-    res.send("vamb33g server is running")
-})
-
+app.get('/', (req, res) => {
+  res.send('vamb33g server is running');
+});
 
 //post route for making requests
-app.post('/', async (req, res)=>{
-    const {message} = req.body
+app.post('/', async (req, res) => {
+  const { message } = req.body;
 
-    try{
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${message}`,
-            max_tokens: 100,
-            temperature: .5
-        })
-        res.json({message: response.data.choices[0].text})
-
-    }catch(e){
-        console.log(e)
-        res.send(e).status(400)
-    }
-})
+  try {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${message}`,
+      max_tokens: 100,
+      temperature: 0.5,
+    });
+    res.json({ message: response.data.choices[0].text });
+  } catch (e) {
+    console.log(e);
+    res.send(e).status(400);
+  }
+});
